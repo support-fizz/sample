77b8ea105f94f74359bd8774cd977ca6f3d053a3
Peter Kelly <peter@fizzsoftware.com>
2024-04-09 09:51:37 +0100

    Initial Commit

---
+++ sample/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  sample
+//
+//  Created by Peter Kelly on 09/04/2024.
+//
+
+import UIKit
+import RealityKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var arView: ARView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Load the "Box" scene from the "Experience" Reality File
+        let boxAnchor = try! Experience.loadBox()
+        
+        // Add the box anchor to the scene
+        arView.scene.anchors.append(boxAnchor)
+    }
+}
